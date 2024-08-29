b562f9f3af95d4d8f4a67aafa7392b4c50fbb5bb
IrinaMuravyova <miomir@yandex.ru>
2024-08-29 23:27:26 +0500

    Changed habit cell to custom type, added new items  into this cell

---
+++ Views/HabitTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HabitTableViewCell.swift
+//  DailyFocus
+//
+//  Created by Irina Muravyeva on 29.08.2024.
+//
+
+import UIKit
+
+class HabitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
