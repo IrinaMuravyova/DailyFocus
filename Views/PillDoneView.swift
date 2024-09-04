67f03eb865bdd2fc1c7c1bbbd7e2fd3523aae103
IrinaMuravyova <miomir@yandex.ru>
2024-09-04 22:31:44 +0500

    Added doneView for pills

---
+++ Views/PillDoneView.swift
@@ -0,0 +1,8 @@
+//
+//  PillDoneView.swift
+//  DailyFocus
+//
+//  Created by Irina Muravyeva on 04.09.2024.
+//
+
+import Foundation
---
+++ Views/PillsImagesView.swift
@@ -0,0 +1,20 @@
+//
+//  PillsImagesView.swift
+//  DailyFocus
+//
+//  Created by Irina Muravyeva on 04.09.2024.
+//
+
+import UIKit
+
+class PillsImagesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
